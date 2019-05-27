f202056adaf793a220a6a8e59a42c118d1802418
Hilary <hilary.scott90@gmail.com>
2019-05-27 19:25:36 -0400

    added home page

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var newFriends = [
+    {
+        "name": "Keanu Reeves",
+        "photo": "https://en.wikipedia.org/wiki/Keanu_Reeves#/media/File:Keanu_Reeves_(crop_and_levels)_(cropped).jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ], 
+    }
+];
+
+module.exports = Friends
